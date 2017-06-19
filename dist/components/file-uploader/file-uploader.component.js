58a1059f3ba1b28fba8033e1846965b7f4b1b2fc
Emmanuel Nyachoke <achachiez@gmail.com>
2017-06-19 09:43:34 +0300

    Release 0.0.1

---
+++ dist/components/file-uploader/file-uploader.component.js
@@ -0,0 +1,88 @@
+import { Component, Input, forwardRef, Output, EventEmitter } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+var noop = function () {
+};
+var FileUploderComponent = (function () {
+    function FileUploderComponent() {
+        this.uploadStarted = new EventEmitter();
+        this.uploadCompleted = new EventEmitter();
+        this.uploading = false;
+        // The internal data model
+        this.innerValue = '';
+        // Placeholders for the callbacks which are later providesd
+        // by the Control Value Accessor
+        this.onTouchedCallback = noop;
+        this.onChangeCallback = noop;
+    }
+    Object.defineProperty(FileUploderComponent.prototype, "value", {
+        // get accessor
+        get: function () {
+            return this.innerValue;
+        },
+        // set accessor including call the onchange callback
+        set: function (v) {
+            if (v !== this.innerValue) {
+                this.innerValue = v;
+                this.onChangeCallback(v);
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    // Current time string.
+    FileUploderComponent.prototype.writeValue = function (value) {
+        if (value !== this.innerValue) {
+            this.innerValue = value;
+        }
+    };
+    // From ControlValueAccessor interface
+    FileUploderComponent.prototype.registerOnChange = function (fn) {
+        this.onChangeCallback = fn;
+    };
+    // From ControlValueAccessor interface
+    FileUploderComponent.prototype.registerOnTouched = function (fn) {
+        this.onTouchedCallback = fn;
+    };
+    FileUploderComponent.prototype.onBlur = function () {
+        this.onTouchedCallback();
+    };
+    FileUploderComponent.prototype.onChange = function (event) {
+        var _this = this;
+        var files = event.srcElement.files;
+        var formData = new FormData();
+        formData.append('files', files[0], files[0].name);
+        this.uploading = true;
+        this.uploadStarted.emit({ status: 'upload_start' });
+        this.source(formData).subscribe(function (response) {
+            _this.uploading = false;
+            _this.value = response.path;
+            _this.uploadStarted.emit({ status: 'upload_complete' });
+        });
+    };
+    FileUploderComponent.prototype.clear = function () {
+        this.value = null;
+    };
+    return FileUploderComponent;
+}());
+export { FileUploderComponent };
+FileUploderComponent.decorators = [
+    { type: Component, args: [{
+                selector: 'file-uploader',
+                styles: ["\n    @import url(\"https://fonts.googleapis.com/css?family=Roboto:100\");.tick-tock-time{font-size:2em;font-family:'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif}.btn-file{position:relative;overflow:hidden}.btn-file input[type=file]{position:absolute;top:0;right:0;min-width:100%;min-height:100%;font-size:100px;text-align:right;filter:alpha(opacity=0);opacity:0;outline:none;background:white;cursor:inherit;display:block}#img-upload{width:100%}.image-preview-input{position:relative;overflow:hidden;margin:0px;color:#333;background-color:#fff;border-color:#ccc}.image-preview-input input[type=file]{position:absolute;top:0;right:0;margin:0;padding:0;font-size:20px;cursor:pointer;opacity:0;filter:alpha(opacity=0)}.image-preview-input-title{margin-left:2px}\n  "],
+                template: "\n    <div class=\"col-md-12\">\n      <div class=\"form-group\">\n        <label>Upload Image</label>\n        <div class=\"input-group\">\n          <input type=\"text\" class=\"form-control\" readonly [(ngModel)]=\"value\">\n          <span class=\"input-group-btn\">\n            <button *ngIf=\"value\" type=\"button\" (click)=\"clear()\" class=\"btn btn-default image-preview-clear\">\n                            <span class=\"glyphicon glyphicon-remove\"></span> Clear\n          </button>\n          <div class=\"btn btn-default image-preview-input\">\n            <span class=\"glyphicon glyphicon-folder-open\"></span>\n            <span class=\"image-preview-input-title\">Browse</span>\n            <input type=\"file\" accept=\"image/png, image/jpeg, image/gif\" (blur)=\"onBlur()\" name=\"input-file-preview\" (change)=\"onChange($event)\"\n            />\n            <!-- rename it -->\n          </div>\n          </span>\n        </div>\n        <span *ngIf=\"uploading\">uploading please wait......</span>\n        <img src={{value}} />\n      </div>\n    </div>\n  ",
+                providers: [
+                    {
+                        provide: NG_VALUE_ACCESSOR,
+                        useExisting: forwardRef(function () { return FileUploderComponent; }), multi: true
+                    }
+                ]
+            },] },
+];
+/** @nocollapse */
+FileUploderComponent.ctorParameters = function () { return []; };
+FileUploderComponent.propDecorators = {
+    'source': [{ type: Input },],
+    'uploadStarted': [{ type: Output },],
+    'uploadCompleted': [{ type: Output },],
+};
+//# sourceMappingURL=file-uploader.component.js.map
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+export { FileUploaderService } from './services';
+export { FileUploderComponent } from './components';
+export { FileUploaderModule } from './file-uploader.module';
+//# sourceMappingURL=index.js.map
