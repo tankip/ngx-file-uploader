b5e509fbff949f6ae0f1cd5dabc71eae53e33412
Emmanuel Nyachoke <achachiez@gmail.com>
2017-07-20 14:00:05 +0300

    Release 0.0.9

---
+++ dist/services/file-uploader/file-uploader.service.js
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+/**
+ * TickTockService class.
+ */
+var FileUploaderService = (function () {
+    function FileUploaderService() {
+    }
+    FileUploaderService.decorators = [
+        { type: Injectable },
+    ];
+    /** @nocollapse */
+    FileUploaderService.ctorParameters = function () { return []; };
+    return FileUploaderService;
+}());
+export { FileUploaderService };
+//# sourceMappingURL=file-uploader.service.js.map
